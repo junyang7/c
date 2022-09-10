4df628b1dcbdc65c72af3a894e908ffa147c6e80
junyang7 <junyang7@staff.weibo.com>
2022-09-10 22:10:09 +0800

    提交

--- src/C.php
+++ src/C.php
@@ -2,10 +2,16 @@
 
 namespace Junyang7\C;
 
+use Junyang7\B\B;
+
 class C
 {
     public static function doC()
     {
         var_dump("C:\t" . date("Y-m-d H:i:s"));
+    }
+    public static function doB()
+    {
+        B::doB();
     }
 }
